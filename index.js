1294010f136e000b30d81e7a67c9e20b8f193493
Casper Kessels <casperkessels@gmail.com>
2021-02-23 21:11:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var ajaxQueue = new Array();
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2Fza2VzIiwiYSI6ImNqYW1tNGdwdjN3MW8yeWp1cWNsaXZveDYifQ.MNpL7SYvoVgR4s_4ma5iyg';
 var map = new mapboxgl.Map({
     container: 'map', // container id
-    style: 'mapbox://styles/stevage/ciywkc4dg000l2rmfzqopmwd1', // stylesheet location
+    style: 'mapbox://styles/caskes/cklif3pxu1hjr17mdwt73pvqe', // stylesheet location
     center: [2.32008, 48.85578], // starting position [lng, lat]
     zoom: 15,
     hash: true
