9329e9bb65d0fee03e78aa5e5e1550e8b5125d95
Casper Kessels <casperkessels@gmail.com>
2021-07-19 19:47:01 +0200

    enlarge article text

--- buttons.js
+++ buttons.js
@@ -60,12 +60,18 @@ function hideInfopanel() {
   }
 }
 
-function toggleBiginfopanel() {
-
+function toggleBigimage() {
   if ($(".article-image").hasClass('hide')) {
     if ($(window).width() > 960) {
       infoTwothirds();
       mapOnethird();
+      $(".article-image").animate({
+        'min-height': "500px",
+      }, 200, function () {
+        // Animation complete.
+      });
+      document.getElementById("wiki-intro").style.width = "50%";
+      document.getElementById("wikilinks").style.width = "50%";
     }
     $(".article-image").removeClass('hide').addClass('show');
   }
@@ -73,11 +79,52 @@ function toggleBiginfopanel() {
     if ($(window).width() > 960) {
       infoOnethird();
       mapTwothirds();
+      $(".article-image").animate({
+        'min-height': "300px",
+      }, 200, function () {
+        // Animation complete.
+      });
+      document.getElementById("wiki-intro").style.width = "100%";
+      document.getElementById("wikilinks").style.width = "100%";
     }
     $(".article-image").removeClass('show').addClass('hide');
   }
-  
 }
+
+function toggleBigtext() {
+  if ($(".wiki-intro").hasClass('hide')) {
+    if ($(window).width() > 960) {
+      infoTwothirds();
+      mapOnethird();
+      $(".wiki-intro").animate({
+        // 'width': "calc(50% - 16px)",
+        'marginLeft': "16px"
+      }, 200, function () {
+        // Animation complete.
+      });
+      document.getElementById("wiki-intro").style.width = "calc(50% - 16px)";
+      document.getElementById("articleimage").style.maxWidth = "50%";
+      document.getElementById("wikilinks").style.width = "50%";
+    }
+    $(".wiki-intro").removeClass('hide').addClass('show');
+  }
+  else {
+    if ($(window).width() > 960) {
+      infoOnethird();
+      mapTwothirds();
+      $(".wiki-intro").animate({
+        'marginLeft': "0px"
+      }, 200, function () {
+        // Animation complete.
+      });
+      document.getElementById("articleimage").style.maxWidth = "100%";
+      document.getElementById("wiki-intro").style.width = "100%";
+      document.getElementById("wikilinks").style.width = "100%";
+    }
+    $(".wiki-intro").removeClass('show').addClass('hide');
+  }
+}
+
 
 
 
@@ -121,14 +168,6 @@ function infoOnethird() {
   }, 200, function () {
     // Animation complete.
   });
-  $(".article-image").animate({
-    'min-height': "300px",
-  }, 200, function () {
-    // Animation complete.
-  });
-  document.getElementById("wiki-intro").style.width = "100%";
-  document.getElementById("wikilinks").style.width = "100%";
-  document.getElementById("article-links").style.flexDirection = "column";
 }
 
 function infoTwothirds() {
@@ -138,13 +177,6 @@ function infoTwothirds() {
     }, 200, function () {
       // Animation complete.
     });
-    $(".article-image").animate({
-      'min-height': "500px",
-    }, 200, function () {
-      // Animation complete.
-    });
-    document.getElementById("wiki-intro").style.width = "50%";
-    document.getElementById("wikilinks").style.width = "50%";
-    document.getElementById("article-links").style.flexDirection = "row";
+    
 
 }
